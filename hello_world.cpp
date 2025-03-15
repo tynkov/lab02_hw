3bce5f08a038cbd4bf39d76296edcd89f9587f8d
tynkov <andrey.tynkov@gmail.com>
2025-03-15 14:33:32 -0400

    clang-format applied to  hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <string>
 
-int main() {
-  std::string name;							//сreating variable
-  std::cout << "Please, enter your name: ";				
-  std::cin >> name;							//чтение переменной из консоли
-  std::cout << std::endl << "Hello world from " << name << std::endl;	//вывод сообщения в консоль
+int
+main()
+{
+  std::string name; // creating variable
+  std::cout << "Please, enter your name: ";
+  std::cin >> name; // чтение переменной из консоли
+  std::cout << std::endl
+            << "Hello world from " << name
+            << std::endl; // вывод сообщения в консоль
 }
