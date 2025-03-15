890d9ce391cec81b81312ee577dcabbe16a9144d
tynkov <52075108+tynkov@users.noreply.github.com>
2025-03-15 14:37:51 -0400

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main() {
-  std::string name;							//создание переменной
+  std::string name;							//сreating variable
   std::cout << "Please, enter your name: ";				
   std::cin >> name;							//чтение переменной из консоли
   std::cout << std::endl << "Hello world from " << name << std::endl;	//вывод сообщения в консоль
