6f12833b31200ee13fac2f8adff6ca6b8a2914ec
tynkov <andrey.tynkov@gmail.com>
2025-03-15 09:20:31 -0400

    added name input in hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
-  cout << "Hello, World!" << endl;
+  string name;
+  cout << "Please, enter your name: ";
+  cin >> name;
+  cout << endl << "Hello world from " << name << endl;
 }
